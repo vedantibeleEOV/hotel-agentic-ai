e119fbaa15b8e310b411d3c6575512d52955d471
vedantibeleEOV <vedanti.bele@embarkingonvoyage.com>
2026-09-16 17:56:44 +0530

    Remove obsolete files and update router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,13 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import health, agent, gemeni, hotel_stuff
+from app.api.v1.endpoints import health
 
 api_router = APIRouter()
 
 # Include health routes
 api_router.include_router(health.router)
 
-# Include agent routes
-api_router.include_router(agent.router)
-
-api_router.include_router(gemeni.router)
-api_router.include_router(hotel_stuff.router)
